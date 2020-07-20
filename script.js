20c105116adbd0fff0248d63ec5d30f381792f22
Cristian Orozco <cristianorozco1984@gmail.com>
2020-07-19 23:02:47 -0500

    expressions recognition

--- script.js
+++ script.js
@@ -21,4 +21,22 @@ Promise.all([
 	faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
 	faceapi.nets.faceExpressionNet.loadFromUri('/models'),
 	faceapi.nets.ageGenderNet.loadFromUri('/models'),
-])
+]).then(cameraOn)
+
+video.addEventListener('play', () => {
+	const canvas = faceapi.createCanvasFromMedia(video)
+	document.body.append(canvas)
+	const displaySize = { width: video.width, height: video.height }
+	faceapi.matchDimensions(canvas, displaySize)
+	setInterval(async () => {
+		const detections = await faceapi
+			.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+			.withFaceLandmarks()
+			.withFaceExpressions()
+		const resizeDetections = faceapi.resizeResults(detections, displaySize)
+		canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+		faceapi.draw.drawDetections(canvas, resizeDetections)
+		faceapi.draw.drawFaceLandmarks(canvas, resizeDetections)
+		faceapi.draw.drawFaceExpressions(canvas, resizeDetections)
+	}, 100)
+})
